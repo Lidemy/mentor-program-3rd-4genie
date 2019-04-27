fb1c8d34f69a708cb1e69bdc06180cae0fbb2a75
Genie <forgenie4all@gmail.com>
2019-04-27 19:39:19 +0800

    week2 hw1 done!

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,10 @@
+// eslint-disable-next-line space-before-function-paren
 function printStars(n) {
-  console.log(n);
+  // 設定變數 starts 為'*'
+  const starts = '*'
+  for (let i = 0; i < n; i += 1) {
+    console.log(starts)
+  }
 }
 
-printStars(5);
+printStars(5)
