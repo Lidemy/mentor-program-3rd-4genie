a6b5c45a8dd44c10bb52b59c112af5ebbbb1bf41
Genie <forgenie4all@gmail.com>
2019-04-27 23:26:03 +0800

    week2 hw5 done!

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,9 +1,18 @@
 function join(str, concatStr) {
-  return concatStr;
+  let newStr = '';
+  if (str.length > 1) {
+    for (let i = 0; i < str.length - 1; i += 1) {
+      newStr += str[i] + concatStr;
+    } return newStr + str[str.length - 1];
+  }
+  return str + concatStr;
 }
 
 function repeat(str, times) {
-  return times;
+  let newStr = '';
+  for (let i = 1; i <= times; i += 1) {
+    newStr += str;
+  } return newStr;
 }
 
 console.log(join('a', '!'));
