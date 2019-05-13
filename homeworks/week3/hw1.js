2b80c23b79f6f24d753e312c177d70326cd35678
Genie <forgenie4all@gmail.com>
2019-05-13 23:39:46 +0800

    week3 hw1 done

--- homeworks/week3/hw1.js
+++ homeworks/week3/hw1.js
@@ -1,5 +1,12 @@
 function stars(n) {
-  console.log(n);
+  const result = [];
+  let storeStars = '';
+  for (let i = 1; i <= n; i += 1) {
+    storeStars += '*';
+    result.push(storeStars);
+  }
+  return result;
 }
 
+console.log(stars(7));
 module.exports = stars;
